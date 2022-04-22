eaa84b443ca1c5ce527abb2d29eb730b329e8635
TsubasaYeung <865500815@qq.com>
2022-04-22 17:24:07 +0800

    add_users.sh del_users.sh accept arguments USER_LIST

--- snippets/shell/add_users.sh
+++ snippets/shell/add_users.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+USER_LIST=$1
 USER_FILE=./user.profile
-for USER in user{1..10}; do
+# for USER in user{1..10}; do
+for USER in $USER_LIST; do
 	if ! id $USER &>/dev/null; then
-		PASS=$(echo $RANDOM | md5sum | cut -c 9-24)
+		PASS=$(echo $RANDOM | md5sum | cut -c 9-16)
 		useradd $USER
 		echo $PASS | passwd --stdin $USER
 		echo "$USER  $PASS" >> $USER_FILE
--- snippets/shell/del_users.sh
+++ snippets/shell/del_users.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+USER_LIST=$1
 USER_FILE=./user.profile
-for USER in user{1..10}; do
+# for USER in user{1..10}; do
+for USER in $USER_LIST; do
 	if id $USER &>/dev/null; then
 		userdel -rf $USER
 		sed -i "/^$USER  /d" $USER_FILE
