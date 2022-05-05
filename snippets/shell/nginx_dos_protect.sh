9454fe0f77e783138c4c4a196522f39a518c401b
TsubasaYeung <865500815@qq.com>
2022-05-05 12:16:38 +0800

    nginx_dos_protect.sh

--- snippets/shell/nginx_dos_protect.sh
+++ snippets/shell/nginx_dos_protect.sh
@@ -3,10 +3,12 @@
 LOG_DIR=/usr/local/nginx/logs
 LOG_FILES="access.log"
 
-ABNORMAL_IP=$(tail -n5000 $LOG_DIR/$LOG_FILES | grep -P "\[$(date +%d/%b/%Y:%H:%M):\\d{2}" | awk '{a[$1]++}END{for(i in a)if(a[i]>100)print i}')
+ABNORMAL_IP=$(tail -n5000 $LOG_DIR/$LOG_FILES | grep -P "\[$(date +%d/%b/%Y:%H:%M):\\d{2}" | awk '{a[$1]++}END{for(i in a)if(a[i]>10)print i}')
 for IP in $ABNORMAL_IP; do
 	if [ $(iptables -vnL | grep -c "$IP") -eq 0 ]; then
 		iptables -I INPUT -s $IP -j DROP
+		# 恢复访问权限
+		# iptables -D INPUT -s $IP -j DROP
 		echo "$(date +'%F_%T') $IP" >> /tmp/drop_ip.log
 	fi
 done
