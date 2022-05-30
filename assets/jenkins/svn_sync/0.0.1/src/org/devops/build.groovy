db7abcb9253e70bc5f4e471bac182b84404413c3
TsubasaYeung <tsubasayeung@gmail.com>
2022-05-30 12:23:44 +0800

    [0] jenkins - 资源修改检测

--- assets/jenkins/svn_sync/0.0.1/src/org/devops/build.groovy
+++ assets/jenkins/svn_sync/0.0.1/src/org/devops/build.groovy
@@ -530,8 +530,10 @@ def pub200AutomaticIntegrated() {
         // 检出代码
         checkoutSVN(params.HG_REPOSITORY_SRC)
 
-        // 资源修改检测
-        bat([label: '资源修改检测', returnStdout: false, script: "node %WORKSPACE%/automator/main.js %WORKSPACE%/automator/cfg/dldl/monitor_resource_modification.yml --FULL_AUTOMATIC --workspaceFolder %WORKSPACE%/project --revisions \"${getRevisions()}\" --jenkins ${JENKINS_URL} --webhook https://oapi.dingtalk.com/robot/send?access_token=d49fdc03b05ac8d52da7ad4167b94823a2c77225bb93d943440a0340db5dd313"])
+        if (!params.HG_QUIET) {
+            // 资源修改检测
+            bat([label: '资源修改检测', returnStdout: false, script: "node %WORKSPACE%/automator/main.js %WORKSPACE%/automator/cfg/dldl/monitor_resource_modification.yml --FULL_AUTOMATIC --workspaceFolder %WORKSPACE%/project --revisions \"${getRevisions()}\" --jenkins ${JENKINS_URL} --webhook https://oapi.dingtalk.com/robot/send?access_token=d49fdc03b05ac8d52da7ad4167b94823a2c77225bb93d943440a0340db5dd313"])
+        }
 
         // 编译
         if (needCompile()) {
