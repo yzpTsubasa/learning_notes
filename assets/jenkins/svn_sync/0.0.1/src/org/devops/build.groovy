9c8df01c4e11505df6f3b3bd21111c643ab3f0fa
TsubasaYeung <tsubasayeung@gmail.com>
2023-03-08 17:11:29 +0800

    [0] jenkins - 限制记录条数上限为10条

--- assets/jenkins/svn_sync/0.0.1/src/org/devops/build.groovy
+++ assets/jenkins/svn_sync/0.0.1/src/org/devops/build.groovy
@@ -6,7 +6,7 @@ import org.yaml.snakeyaml.Yaml
 def getChangeString() {
     MAX_MSG_LEN = 500
     // echo 'Gathering SCM changes......'
-    def MAX_ITEMS = 100 // 限制记录条数上限为100条
+    def MAX_ITEMS = 10 // 限制记录条数上限为10条
     def isExceeded = false // 是否超过上限条数
     def numItem = 0
     return currentBuild.changeSets.collect {
