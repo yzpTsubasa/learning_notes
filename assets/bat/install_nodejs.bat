770f3ea47d5d9f73c34af9b7f6485eb9d62a9907
TsubasaYeung <tsubasayeung@gmail.com>
2024-05-12 11:17:09 +0800

    优化 nodejs 安装、卸载脚本

--- assets/bat/install_nodejs.bat
+++ assets/bat/install_nodejs.bat
@@ -1,7 +1,30 @@
 @echo off
 setlocal enabledelayedexpansion
 
-call admin.bat
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
 
 set NODE_VERSION=16.20.0
 @REM set NODE_URL=https://nodejs.org/dist/v%NODE_VERSION%/node-v%NODE_VERSION%-x64.msi
