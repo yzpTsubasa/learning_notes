a9f3c43280d9d08b5d6cf2ab83aac0960bdd71c1
Tsubasa Yeung <tsubasayeung@gmail.com>
2024-05-12 09:39:42 +0800

    install_nodejs - 优化判断

--- assets/bat/install_nodejs.bat
+++ assets/bat/install_nodejs.bat
@@ -5,8 +5,8 @@ setlocal
 %1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
 
 set NODE_VERSION=16.20.0
-@REM set NODE_URL=https://nodejs.org/dist/v%NODE_VERSION%/node-v%NODE_VERSION%-x64.msi
-set NODE_URL=https://registry.npmmirror.com/-/binary/node/v%NODE_VERSION%/node-v%NODE_VERSION%-x64.msi
+set NODE_URL=https://nodejs.org/dist/v%NODE_VERSION%/node-v%NODE_VERSION%-x64.msi
+@REM set NODE_URL=https://registry.npmmirror.com/-/binary/node/v%NODE_VERSION%/node-v%NODE_VERSION%-x64.msi
 set NODE_MSI=%TEMP%\node-v%NODE_VERSION%-x64.msi
 
 echo 开始下载Node.js安装程序...
@@ -15,10 +15,12 @@ bitsadmin /transfer "下载Node.js安装程序" /download /priority normal %NODE_URL% 
 echo 下载完成，开始安装Node.js...
 msiexec /i "%NODE_MSI%" /quiet /qn /norestart
 
-if exist "%PROGRAMFILES%\nodejs\node.exe" (
-    echo 安装成功，Node.js已安装到 %PROGRAMFILES%\nodejs\node.exe
+@REM 判断是否有 npx 命令
+where npx >nul 2>&1
+if not %ERRORLEVEL% == 0 (
+    echo 安装失败，请手动安装Node.js。
 ) else (
-    echo 安装失败，找不到 %PROGRAMFILES%\nodejs\node.exe
+    echo 安装成功!
 )
 pause
 endlocal
