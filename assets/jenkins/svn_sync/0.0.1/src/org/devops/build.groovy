1c490adbf75d920805886976f463cb3b6e46e9b2
TsubasaYeung <tsubasayeung@gmail.com>
2024-06-13 09:53:16 +0800

    [0] jenkins - 通用通知显示发起者

--- assets/jenkins/svn_sync/0.0.1/src/org/devops/build.groovy
+++ assets/jenkins/svn_sync/0.0.1/src/org/devops/build.groovy
@@ -169,6 +169,7 @@ def sendResult2DingTalkSimple() {
         text: [
             "- 任务 [${currentBuild.fullDisplayName}](${BUILD_URL}) ",
             "- 状态 <font color=${result_color}>${result}</font>",
+            "- 发起 ${getRootBuildTriggerDesc()}",
             "- 时刻 ${new Date().format('yyyy-MM-dd(E)HH:mm:ss', TimeZone.getTimeZone('Asia/Shanghai')) - '星期'}",
         ]
     )
@@ -576,12 +577,6 @@ def sendCommonResult2DingTalk() {
         return
     }
     resolveResult()
-    def buildTriggerDesc = "Unknown"
-    try {
-        buildTriggerDesc = getRootBuildTriggerDesc()
-    } catch (Exception e) {
-        print(e)
-    }
     env.durationString = currentBuild.durationString.minus(' and counting')
     dingtalk(
         robot: getDingTalkRobot(),
@@ -593,7 +588,7 @@ def sendCommonResult2DingTalk() {
             "# **[${currentBuild.fullDisplayName}](${BUILD_URL})**",
             '***',
             "- 状态 <font color=${result_color}>${result}</font>",
-            "- 发起 ${buildTriggerDesc}",
+            "- 发起 ${getRootBuildTriggerDesc()}",
             "- 时刻 ${new Date().format('yyyy-MM-dd(E)HH:mm:ss', TimeZone.getTimeZone('Asia/Shanghai')) - '星期'}",
             "- 用时 ${durationString}",
             env.HG_PATCH_FILE ? "- [点击查看修改](${env.HG_PATCH_FILE})" : "",
@@ -1054,12 +1049,17 @@ def hasIndexJS2Refresh() {
 
 // 获取最上游构建的发起描述
 def getRootBuildTriggerDesc() {
-    def build = getRootBuild(currentBuild)
-    def desc = build.getBuildCauses()[0] && (build.getBuildCauses()[0].userName ? build.getBuildCauses()[0].userName : build.getBuildCauses()[0].shortDescription.minus('Started by ').replace('timer', '定时器').replace('an SCM change', 'SCM轮询'))
-    if (build.getAbsoluteUrl() != currentBuild.getAbsoluteUrl()) {
-        desc += "[${build.getFullDisplayName()}](${build.getAbsoluteUrl()})"
+    try {
+        def build = getRootBuild(currentBuild)
+        def desc = build.getBuildCauses()[0] && (build.getBuildCauses()[0].userName ? build.getBuildCauses()[0].userName : build.getBuildCauses()[0].shortDescription.minus('Started by ').replace('timer', '定时器').replace('an SCM change', 'SCM轮询'))
+        if (build.getAbsoluteUrl() != currentBuild.getAbsoluteUrl()) {
+            desc += "[${build.getFullDisplayName()}](${build.getAbsoluteUrl()})"
+        }
+        return desc
+    } catch (Exception e) {
+        print(e)
     }
-    return desc
+    return "Unknown"
 }
 
 // 获取最上游构建的发起人id
